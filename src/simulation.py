867b5ff6d3b3229ef218f896ab1b460b5a3bc387
Niall Cardin <NiallC@gmail.com>
2025-07-02 14:47:53 +0100

    Building out a simple simulation loop.

--- src/simulation.py
+++ src/simulation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from src.population import Population
 
 class Simulation:
@@ -35,5 +36,38 @@ class Simulation:
                                      fitness_function=fitness_function,
                                      random_seed=random_seed)
         self.results = []  # Will hold per-generation statistics as dicts
+
+    def run(self):
+        """
+        Run the simulation for the specified number of generations.
+        Collects per-generation statistics: generation, mean fitness, mean heterozygosity.
+        """
+        self.results = []
+        for gen in range(self.generations + 1):
+            # Collect statistics
+            fitnesses = self.population.fitness()
+            mean_fitness = np.mean(fitnesses)
+            # Heterozygosity: mean fraction of heterozygous sites across the population
+            # For binary genomes, heterozygosity at each site is 2p(1-p), where p is freq of 1s
+            genomes_matrix = np.array([g.sequence for g in self.population.genomes])
+            p_ones = np.mean(genomes_matrix, axis=0)
+            heterozygosity = np.mean(2 * p_ones * (1 - p_ones))
+            self.results.append({
+                'generation': gen,
+                'mean_fitness': mean_fitness,
+                'mean_heterozygosity': heterozygosity
+            })
+            # Evolve population (skip on last generation)
+            if gen < self.generations:
+                if self.mode == 'asexual':
+                    self.population = self.population.evolve_asexual(self.mutation_rate)
+                elif self.mode == 'sexual':
+                    self.population = self.population.evolve_sexual(self.mutation_rate)
+
+    def get_results(self):
+        """
+        Return the results as a pandas DataFrame.
+        """
+        return pd.DataFrame(self.results)
 
     # Main loop and results collection to be implemented in the next step 
